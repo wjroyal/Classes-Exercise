40bfb0bb3c70755d028575413e2ca7dda4abcd49
wjroyal <125611505+wjroyal@users.noreply.github.com>
2023-09-21 23:57:38 -0700

    Add files via upload

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Jeep";
+            myCar.Model = "Patriot Sport";
+            myCar.Year = 2017;
+
+            var myMotorcycle = new Car();
+            myCar.Make = "Harley Davidson";
+            myCar.Model = "Dyna FXDF";
+            myCar.Year = 2009;
+
+            var carList = new List<Car>() { myCar, myMotorcycle};
+
+            Console.WriteLine(carList.Capacity);
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
         }
     }
 }
